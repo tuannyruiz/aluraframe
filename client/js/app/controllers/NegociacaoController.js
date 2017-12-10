7bef277e0b36e958194d8cdf1c7a47b66513cfdb
Tuanny Ruiz <tuannyruiz@gmail.com>
2017-12-10 21:50:09 -0200

    Create NegociacaoController class

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,15 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+
+    }
+}
