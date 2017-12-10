a9b112e297ec7164b6f58499535d45e22bb7c173
Tuanny Ruiz <tuannyruiz@gmail.com>
2017-12-10 21:26:53 -0200

    Use getters in Negociacao class

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,7 +1,25 @@
 class Negociacao {
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    constructor(data, quantidade, valor) {
+        //o 'this' aponta para a instância que está utilizando a classe
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
     }
 }
