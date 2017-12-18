aa4a9f32ed2fec71f9fee61eb59ce954930fa204
Tuanny Ruiz <tuannyruiz@gmail.com>
2017-12-18 20:01:57 -0200

    Fix date bug

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,14 +2,25 @@ class NegociacaoController {
 
     constructor() {
         let $ = document.querySelector.bind(document);
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
     adiciona(event) {
         event.preventDefault();
 
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map(function(item, indice) {
+                if(indice == 1) {
+                    return item - 1;
+                }
+                return item;
+            })
+        );
 
+        console.log(data);
     }
 }
